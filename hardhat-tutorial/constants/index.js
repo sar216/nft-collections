272868a477cd61f8b3d2a92fddd940ea052c58e8
chihab216 <chihab.khnifass@mines-ales.org>
2023-02-12 22:22:48 +0100

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xFE74cC0aB9823a85228831e17179BBA5647f32dc";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
